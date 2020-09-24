980f914f9ef7df9542b22a7bc2b83bc4745866fc
Jeremy <cyberchefjay@gmail.com>
2020-09-24 05:43:52 -0500

    Bump version: 0.1.0-beta1 → 0.1.0-beta2

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'chichi:chichi', // https://atmospherejs.com/chichi-framework/chichi
   summary: 'The most popular front-end framework for Stylus',
-  version: '0.1.0-beta1',
+  version: '0.1.0-beta2',
   git: 'https://github.com/chichi-framework/chichi.git'
 });
 
