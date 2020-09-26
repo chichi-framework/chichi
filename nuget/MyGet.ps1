28f60535432577d44712a6236c195965a855a9cd
Jeremy <cyberchefjay@gmail.com>
2020-09-25 21:33:52 -0500

    Fixed .appveyor.yml

--- nuget/MyGet.ps1
+++ nuget/MyGet.ps1
@@ -6,14 +6,6 @@ $nuget = $env:NuGet
 
 Copy-Item $env:SourcesPath\LICENSE $env:SourcesPath\LICENSE.txt # has to be .txt extension, don't check in
 
-# parse the version number out of package.json
-$versionParts = ((Get-Content $env:SourcesPath\package.json) -join "`n" | ConvertFrom-Json).version.split('-', 2) # split the version on the '-'
-$version = $versionParts[0]
-
-if ($versionParts.Length -gt 1) {
-  $version += '-' + $versionParts[1].replace('.', '').replace('-', '_') # strip out invalid chars from the PreRelease part
-}
-
 # create packages
-& $nuget pack "$env:SourcesPath\nuget\chichi.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath -Version $version
-& $nuget pack "$env:SourcesPath\nuget\chichi.stylus.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath -Version $version
+& $nuget pack "$env:SourcesPath\nuget\chichi.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath
+& $nuget pack "$env:SourcesPath\nuget\chichi.stylus.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath
