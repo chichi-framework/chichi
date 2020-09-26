d1321812fff33a85f390ebfcd3aa55e94a66a671
Jeremy <cyberchefjay@gmail.com>
2020-09-25 23:52:38 -0500

    Fixed Brand logo on readme

--- nuget/MyGet.ps1
+++ nuget/MyGet.ps1
@@ -6,6 +6,8 @@ $nuget = $env:NuGet
 
 Copy-Item $env:SourcesPath\LICENSE $env:SourcesPath\LICENSE.txt # has to be .txt extension, don't check in
 
+# get version
+
 # create packages
 & $nuget pack "$env:SourcesPath\nuget\chichi.nuspec" -BasePath $env:SourcesPath
 & $nuget pack "$env:SourcesPath\nuget\chichi.stylus.nuspec" -BasePath $env:SourcesPath
