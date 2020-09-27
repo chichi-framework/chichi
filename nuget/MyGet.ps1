8b7fd8e35cfb7e56982dcb00cd38ea9b072d8819
Jeremy <cyberchefjay@gmail.com>
2020-09-27 04:09:19 -0500

    Modified: .nuspec files

--- nuget/MyGet.ps1
+++ nuget/MyGet.ps1
@@ -7,7 +7,12 @@ $nuget = $env:NuGet
 Copy-Item $env:SourcesPath\LICENSE $env:SourcesPath\LICENSE.txt # has to be .txt extension, don't check in
 
 # get version
+$versionParts = ((Get-Content $env:SourcesPath\package.json) -join "`n").version.split('-', 2)
+$version = $versionParts[0]
 
+if ($versionParts.length -gt 1) {
+  $version += '-' + $versionParts[1].replace('.', '').replace('-', '_')
+}
 # create packages
-& $nuget pack "$env:SourcesPath\nuget\chichi.nuspec" -BasePath $env:SourcesPath
-& $nuget pack "$env:SourcesPath\nuget\chichi.stylus.nuspec" -BasePath $env:SourcesPath
+& $nuget pack "$env:SourcesPath\nuget\chichi.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath -Version $version
+& $nuget pack "$env:SourcesPath\nuget\chichi.stylus.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath -Version $version
